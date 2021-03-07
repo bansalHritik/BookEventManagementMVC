4d617e963e697846b89e415100d70877fa7ceeb2
Ritik Bansal <17egjcs135@gitjaipur.com>
2021-03-07 23:52:47 +0530

    Added Event Controller.

---
+++ WebBookEventManager/Controllers/EventsController.cs
@@ -0,0 +1,42 @@
+﻿using DTO.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBookEventManager.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View("EventForm");
+        }
+
+        public ActionResult Save(EventDto eventDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Create");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Manage()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
