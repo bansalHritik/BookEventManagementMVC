2d6ad42a56a7862bf7aed4687a0e9bf54a3733f1
Ritik Bansal <17egjcs135@gitjaipur.com>
2021-03-07 23:54:50 +0530

    Add View Models

---
+++ WebBookEventManager/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Shared.Constants.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebBookEventManager.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public EventFormViewModel()
+        {
+            EventType = EventType.Public;
+        }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required, Display(Name = "Start Time")]
+        public DateTime StartTime { get; set; }
+
+        [Range(0.0, 4.0), Display(Name = "Duration(in hours)")]
+        public byte? DurationInHours { get; set; }
+
+        [StringLength(50)]
+        public string Description { get; set; }
+
+        [StringLength(500), Display(Name = "Other Details")]
+        public string OtherDetails { get; set; }
+
+        [Display(Name = "Event Type")]
+        public EventType EventType { get; set; }
+    }
+}
---
+++ WebBookEventManager/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DTO.DTO;
+
+
+namespace WebBookEventManager.ViewModels
+{
+    public class HomeViewModel
+    {
+        
+    }
+}
