20d78c4a8e2c199b00457976d9cb6f8f2bd9e501
Ritik Bansal <17egjcs135@gitjaipur.com>
2021-03-08 09:24:38 +0530

    Add Event DTO

---
+++ DTO/Events/EventDto.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Constants.Enums;
+
+namespace DTO.Events
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public EventType EventType { get; set; }
+    }
+}
