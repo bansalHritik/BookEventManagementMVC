809e3bcfdaa07103c3b99aeaebfbce1a90d5b125
Ritik Bansal <17egjcs135@gitjaipur.com>
2021-03-08 14:53:09 +0530

    updated Event dto.

--- DTO/Events/EventDto.cs
+++ DTO/Events/EventDto.cs
@@ -1,11 +1,26 @@
 ﻿using Shared.Constants.Enums;
+using System;
 
 namespace DTO.Events
 {
     public class EventDto
     {
         public int Id { get; set; }
+
         public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public byte Duration { get; set; }
+
+        public string Description { get; set; }
+
+        public string OtherDetails { get; set; }
+
         public EventType EventType { get; set; }
     }
 }
